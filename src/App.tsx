5d850ff7e6374f1fbf861ac7486b01c84c637a83
goanrox <Otlwzhelpdesk@gmail.com>
2026-02-26 16:54:15 -0500

    Remove unused import for RescueGuide component

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react';
 import { DeviceBrand } from './types';
 import { DEVICE_INSTRUCTIONS } from './constants';
 import Button from './components/Button';
-import RescueGuide from './components/RescueGuide';
+
 import AppChecker from './components/AppChecker';
 import AdBlockerInfo from './components/AdBlockerInfo';
 import PerformanceTips from './components/PerformanceTips';
